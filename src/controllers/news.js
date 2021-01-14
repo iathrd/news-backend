e745991398279641f09c80a5ac81e57a7020c235
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-01-14 11:03:54 +0800

    fix created news

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -16,15 +16,21 @@ module.exports = {
           image: path,
         };
       }
-      const newsContent = req.body.content
-        .replace(/\./g, "")
-        .replace(/\n/g, "")
-        .split(" ")
-        .join(" ");
-      const readingTime = Math.ceil(newsContent.length / 250);
+      if (req.file === undefined) {
+        const newsContent = req.body.content
+          .replace(/\./g, "")
+          .replace(/\n/g, "")
+          .split(" ")
+          .join(" ");
+        const readingTime = Math.ceil(newsContent.length / 250);
+        req.body = {
+          ...req.body,
+          readingTime,
+        };
+      }
       const data = await validation.createNewsSchema.validateAsync(req.body);
 
-      const sendData = await News.create({ ...data, readingTime, userId });
+      const sendData = await News.create({ ...data, userId });
       if (sendData) {
         response(res, "News created", { data: sendData.dataValues });
       } else {
--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -1,58 +1,32 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
 module.exports = {
-    registerSchema:Joi.object({
-        username:Joi.string()
-        .min(5)
-        .required(),
-        password:Joi.string()
-        .required(),
-        email:Joi.string()
-        .email()
-        .required(),
-        avatar:Joi.string()   
-    }),
-    createNewsSchema:Joi.object({
-        title:Joi.string()
-        .min(5)
-        .trim()
-        .required(),
-        image:Joi.string()
-        .required(),
-        imageDescription:Joi.string()
-        .min(5)
-        .required(),
-        content:Joi.string()
-        .trim()
-        .required()
-    }),
-    editNewsSchema:Joi.object({
-        title:Joi.string()
-        .min(5)
-        .trim(),
-        image:Joi.string(),
-        imageDescription:Joi.string()
-        .min(5),
-        content:Joi.string()
-        .trim(),
-        readingTime:Joi.number()
-    }),
-    createCategorySchema:Joi.object({
-        name:Joi.string()
-        .trim()
-        .min(4)
-        .required(),
-        description:Joi.string()
-        .trim()
-        .min(5)
-        .required(),
-    }),
-    editCategorySchema:Joi.object({
-        name:Joi.string()
-        .trim()
-        .min(4),
-        description:Joi.string()
-        .trim()
-        .min(5)
-    })
-}
+  registerSchema: Joi.object({
+    username: Joi.string().min(5).required(),
+    password: Joi.string().required(),
+    email: Joi.string().email().required(),
+    avatar: Joi.string(),
+  }),
+  createNewsSchema: Joi.object({
+    title: Joi.string().min(5).trim().required(),
+    image: Joi.string(),
+    imageDescription: Joi.string().min(5).required(),
+    content: Joi.string().trim().required(),
+    readingTime: Joi.number(),
+  }),
+  editNewsSchema: Joi.object({
+    title: Joi.string().min(5).trim(),
+    image: Joi.string(),
+    imageDescription: Joi.string().min(5),
+    content: Joi.string().trim(),
+    readingTime: Joi.number(),
+  }),
+  createCategorySchema: Joi.object({
+    name: Joi.string().trim().min(4).required(),
+    description: Joi.string().trim().min(5).required(),
+  }),
+  editCategorySchema: Joi.object({
+    name: Joi.string().trim().min(4),
+    description: Joi.string().trim().min(5),
+  }),
+};
